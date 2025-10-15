aaa36b6f98447c652d6d2fee4289eeee9d928940
Bugbreaker999 <leonardotavaressoares00@gmail.com>
2025-10-15 15:40:58 +0100

    refactor: simplify main.dart and update dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,47 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'theme/app_theme.dart';
 import 'screens/login_screen.dart';
+import 'services/security_service.dart';
+import 'services/windows_secure_window.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Inicializa o window_manager para Windows
+  if (Platform.isWindows) {
+    await windowManager.ensureInitialized();
+    await windowManager.waitUntilReadyToShow();
+  }
+  
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      SecurityService.initSecurity(context);
+    });
+  }
+
+  @override
+  void dispose() {
+    SecurityService.dispose();
+    if (Platform.isWindows) {
+      WindowsSecureWindow.dispose();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +50,9 @@ class App extends StatelessWidget {
       theme: AppTheme.theme,
       home: const LoginScreen(),
       debugShowCheckedModeBanner: false,
+      routes: {
+        '/login': (context) => const LoginScreen(),
+      },
     );
   }
 }
